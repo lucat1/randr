4b4423a43a9ad4c5fac87af8d99022db9a8d7ccb
luca <lucat1@users.noreply.github.com>
2020-02-18 23:18:22 +0100

    Fix wrong import

--- inputs/static.go
+++ inputs/static.go
@@ -3,7 +3,7 @@ package main
 import "github.com/lucat1/randr"
 
 // Static renders a static component
-func Static(_ randr.Props) string {
+func Static(_ randr.Context) string {
   return randr.HTML(`
     <h1>Hello world!</h1>
   `)
