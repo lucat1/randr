c30b1ddb73021c6c7ee6b7af9111614c4cac83aa
luca <lucat1@users.noreply.github.com>
2020-02-18 23:49:32 +0100

    :fire:

--- outputs/test/only-compiles-with-recursive.randr.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/lucat1/randr"	/* This code is auto-generated; DO NOT EDIT */// +build ignore
-
-// Static renders a static component
-func Static(_ randr.Props) string {
-	return `<h1>Only compiles with recursive</h1>`
-
-}
